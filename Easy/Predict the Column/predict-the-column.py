452c9979ac6f57f249e4280dfdab336f0184e14c
Padala Vinay Rohit Reddy <66181890+rohitreddy192@users.noreply.github.com>
2023-11-20 11:25:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Predict the Column/predict-the-column.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def columnWithMaxZeros(self,arr,N):
+        maxi=0
+        idx = -1
+        for i in range(N):
+            cnt = 0
+            for j in range(N):
+                if arr[j][i] == 0:
+                    cnt += 1
+            if maxi<cnt:
+                maxi = cnt
+                idx = i
+        # code here 
+        return idx if maxi!=0 else -1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        arr = []
+        for i in range(N):
+            line = [int(x) for x in input().strip().split()]
+            arr.append(line)
+        ob=Solution()
+        print(ob.columnWithMaxZeros(arr,N))
+
+
+# } Driver Code Ends
