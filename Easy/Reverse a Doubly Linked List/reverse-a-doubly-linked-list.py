b9fb3716fa9ad1049aefa1ac2644b20494efdfee
Padala Vinay Rohit Reddy <66181890+rohitreddy192@users.noreply.github.com>
2023-11-20 11:21:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.py
@@ -0,0 +1,81 @@
+#User function Template for python3
+
+'''
+class Node: 
+    def __init__(self, data): 
+        self.data = data  
+        self.next = None
+        self.prev = None
+'''
+
+class Solution:
+    def reverseDLL(self, head):
+        if head.next is None:
+            return head
+        current = head
+        temp = None
+        while current:
+            temp = current.prev
+            current.prev = current.next
+            current.next = temp
+            current = current.prev
+        return temp.prev
+        
+        #return head after reversing
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node: 
+    def __init__(self, data): 
+        self.data = data  
+        self.next = None
+        self.prev = None
+  
+class DoublyLinkedList: 
+    def __init__(self): 
+        self.head = None
+   
+    def push(self, new_data,tail):
+        if not self.head:
+            self.head=Node(new_data)
+            return self.head
+        Nnode=Node(new_data)
+        Nnode.prev=tail
+        tail.next=Nnode
+        return Nnode
+        
+    def printList(self, node): 
+        while(node is not None): 
+            print (node.data,end=' ') 
+            node = node.next
+            
+
+            
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        
+        
+        dll=DoublyLinkedList()
+        tail=None
+        
+        for e in arr:
+            tail=dll.push(e,tail)
+        
+        ob = Solution()
+        resHead=ob.reverseDLL(dll.head)
+        dll.printList(resHead)
+        print()
+        
+# } Driver Code Ends
